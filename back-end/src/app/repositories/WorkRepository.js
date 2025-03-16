f5a64c9468bb02ab3a489046e4246dffdf63b4e7
leandrogalbarino <leandrokise753@gmail.com>
2025-03-15 22:18:08 -0300

    Atulizando pesquisa de obras.

--- back-end/src/app/repositories/WorkRepository.js
+++ back-end/src/app/repositories/WorkRepository.js
@@ -3,38 +3,38 @@ import { consult } from '../database/connection.js';
 class WorkRepository{
   create(data) {
     const sql = 'INSERT INTO works SET ?;'
-    const mensageError = 'Não foi possível criar.';
-    return consult(sql, data, mensageError)
+    const messageError = 'Não foi possível criar.';
+    return consult(sql, data, messageError)
   }
 
   findAll() {
     const sql = 'SELECT * FROM works;'
-    const mensageError = 'Não foi possível localizar nenhuma obra.';
-    return consult(sql, mensageError);
+    const messageError = 'Não foi possível localizar nenhuma obra.';
+    return consult(sql, messageError);
   }
 
   findById(id) {
     const sql = 'SELECT * FROM works WHERE idWork=?;'
-    const mensageError = `Não foi possível localizar uma obra com id=${id}.`;
-    return consult(sql, id, mensageError);
+    const messageError = `Não foi possível localizar uma obra com id=${id}.`;
+    return consult(sql, id, messageError);
   }
 
   findByName(name) {
-    const sql = 'SELECT * FROM works WHERE name=?';
-    const mensageError = `Nenhuma obra localizada com o nome ${name}`;
-    return consult(sql, name, mensageError);
+    const sql = 'SELECT * FROM works WHERE name like ?';
+    const messageError = `Nenhuma obra localizada com o nome ${name}`;
+    return consult(sql, [`${name}%`], messageError);
   }
 
   update(id, data) {
     const sql = 'UPDATE works SET ? WHERE idWork=?;'
-    const mensageError = `Não foi possível atualizar a obra com id=${id}`;
-    return consult(sql, [data, id], mensageError);
+    const messageError = `Não foi possível atualizar a obra com id=${id}`;
+    return consult(sql, [data, id], messageError);
   }
 
   delete(id) {
     const sql = 'DELETE FROM works WHERE idWork=?;'
-    const mensageError = `Não foi possível deletar a obra com id=${id}`;
-    return consult(sql, id, mensageError);
+    const messageError = `Não foi possível deletar a obra com id=${id}`;
+    return consult(sql, id, messageError);
   }
 }
 
